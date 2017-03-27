2915e1ada1010109a9b20075d6c2158543ce60bd
Chris <catchcoder@gmail.com>
2017-03-27 23:45:24 +0100

    Added setup.sh script ti install using pip the python packages required

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Install packages required using pip
+sudo pip install RPi.GPIO
+sudo pip install requests
+sudo pip install beautifulsoup4
+sudo pip install fake-useragent
+sudo pip install python-vlc
+
+
