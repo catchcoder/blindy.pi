eb363057abb1e6c47efe22bb0a16bb654c7d4313
Chris Hawkins <catchcoder@gmail.com>
2017-04-04 18:39:25 +0000

    Exported the env using pip freeze to requirements.txt

--- setup.sh
+++ setup.sh
@@ -4,10 +4,5 @@
 apt-get install -y mpd mpc
 
 # Install python packages required using pip
-pip install RPi.GPIO
-pip install requests
-pip install beautifulsoup4
-pip install fake-useragent
-# pip install python-vlc
-
+pip install -r requirements.txt
 
