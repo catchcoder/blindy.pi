5638a632edf692432f34ccfa8bcae47c05c15542
Chris Hawkins <catchcoder@gmail.com>
2017-04-04 13:39:37 +0000

    commenting code

--- blindy.pi.py
+++ blindy.pi.py
@@ -9,37 +9,37 @@ ua = UserAgent()
 ua.update()
 
 # use random browser
-headers = ua.random
+HEADERS = ua.random
 
-start_time = time.time()
+START_TIME = time.time()
 import RPi.GPIO as GPIO
 
-btn1 = 23  # next channel
-btn2 = 14  # stop
-btn3 = 25  # turn off blindy pi radio
-btn4 = 12  # volume up
-btn5 = 16  # volume down
-next = 0
+BTN_1 = 23  # next channel
+BTN_2 = 14  # stop
+BTN_3 = 25  # turn off blindy pi radio
+BTN_4 = 12  # volume up
+BTN_5 = 16  # volume down
+NEXT_STREAM = 0
 
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
 # GPIO.setup(led1, GPIO.OUT)
-GPIO.setup(btn1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(btn2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(btn3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(btn4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(btn5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BTN_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BTN_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BTN_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BTN_4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BTN_5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
-soup = ""
+SOUP = ""
 channels = []
-whatson = ""
+WHATS_ON = ""
 channelname = ""
 firstrun = True
-# Url to read
-url = 'http://blindy.tv'
-url2 = 'http://www.radiofeeds.co.uk/mp3.asp'
+# URL to read
+URL = 'http://blindy.tv'
+URL2 = 'http://www.radiofeeds.co.uk/mp3.asp'
 # If payload required
 # payload = {
 #    'q': 'Python',
@@ -47,23 +47,25 @@ url2 = 'http://www.radiofeeds.co.uk/mp3.asp'
 
 
 def loadpage():
-    global start_time
+    """" Get web page and parse wioth beatifulsoup """
+    global START_TIME
     global firstrun
-    global soup
+    global SOUP
     # add time delay to stop dos on blindy.tv website, it will update the
     # channels array only every 1 minute
-    if (int((time.time() - start_time)) <= 60 and firstrun == False):
+    if int((time.time() - START_TIME)) <= 60 and firstrun == False:
         return
-    start_time = time.time()
+    START_TIME = time.time()
     firstrun = False
-    r = requests.get(url, headers)
-    soup = BeautifulSoup(r.text, "html.parser")
+    r = requests.get(URL, HEADERS)
+    SOUP = BeautifulSoup(r.text, "html.parser")
 
 
 def getchannels():
+    """" Search web page and load hrefs into channels array """
     global channels
-    global soup
-    table = soup.find("table")
+    global SOUP
+    table = SOUP.find("table")
     for row in table.findAll("tr"):
         cells = row.findAll("td")
         if len(cells) == 3:
@@ -71,82 +73,87 @@ def getchannels():
 
 
 def getplaying(playing):
+    """ Find whats playing online now"""
     loadpage()
-    global whatson
+    global WHATS_ON
     global channelname
-    table = soup.find("table")
+    table = SOUP.find("table")
     for row in table.findAll("tr"):
         cells = row.findAll("td")
         if len(cells) == 3:
             if (cells[2].find('a').get('href')) == playing:
-                whatson = (cells[1].text)
+                WHATS_ON = (cells[1].text)
                 channelname = (cells[0].text)
-                return (cells[1].text)
+                return cells[1].text
 
 
 def getallhrefs():
-    titles = soup.findAll('a', href=True)
+    """ Get all Hrefs with m3u in the tag """
+    titles = SOUP.findAll('a', href=True)
     for t in titles:
         if 'm3u' in t['href']:
             print(t['href'])
             print(t.string)
 
 
-def waitforbutton():
-    global next
-    global channels
-    global weblink
+def main():
+    """ Main loop to check if and what button is pressed """
+    global NEXT_STREAM
+    global WEB_LINK
 
     while True:
-        if GPIO.input(btn2) == False:
+        if GPIO.input(BTN_2) == False:
             speak("Blindy tv", "stopping")
             subprocess.call(['mpc', 'stop', '-q'])
 
-        if GPIO.input(btn3) == False:
+        if GPIO.input(BTN_3) == False:
             speak("shutting down", "blindy tv pi")
             subprocess.call(['sudo', 'shutdown', '-t', 'now'])
 
-        if GPIO.input(btn4) == False:
+        if GPIO.input(BTN_4) == False:
             speak("volume", "up")
             subprocess.call(['mpc', 'volume', '+1', '-q'])
 
-        if GPIO.input(btn5) == False:
+        if GPIO.input(BTN_5) == False:
             speak("volume", "down")
             subprocess.call(['mpc', 'volume', '-1', '-q'])
 
-        if GPIO.input(btn1) == False:
+        if GPIO.input(BTN_1) == False:
             # testVar = input(
             #    "\nPress enter for next track or press q + enter to quit.")
             # if testVar == "q":
             #subprocess.call(['mpc', 'stop', '-q'])
 
             # break
-            next += 1
-            if next == len(channels):
-                next = 0
+            NEXT_STREAM += 1
+            if NEXT_STREAM == len(channels):
+                NEXT_STREAM = 0
 
-            getplaying(channels[next])
-            weblink = (channels[next])
-            speakwhatson([channelname, whatson, weblink])
-        # if GPIO.input(btn1) == True:
+            getplaying(channels[NEXT_STREAM])
+            WEB_LINK = (channels[NEXT_STREAM])
+            speakwhatson([channelname, WHATS_ON, WEB_LINK])
+        # if GPIO.input(BTN_1) == True:
 
 
 def speakwhatson(channelinfo=[]):
+    """ Speak using whats on and play stream """
     speak("Channel:", channelname)
-    speak("Playing:", whatson)
+    speak("Playing:", WHATS_ON)
     play()
 
 
 def speak(a, b):
+    """ Use espeak to say what the playing or what button was pressed """
     subprocess.call(['mpc', 'stop', '-q'])
     subprocess.call(['espeak', a, '2>/dev/null'])
     subprocess.call(['espeak', b, '2>/dev/null'], stderr=subprocess.STDOUT)
 
 
 def play():
+    """  Using MPC load new stream and play it """
     subprocess.call(['mpc', 'clear', '-q'])
-    subprocess.call(['mpc', 'load', weblink])
-    p = subprocess.Popen(['mpc',
+    subprocess.call(['mpc', 'load', WEB_LINK])
+    subprocess.Popen(['mpc',
                           'play',
                           '-q'],
                          stdin=subprocess.PIPE,
@@ -155,16 +162,18 @@ def play():
 
 
 def startup_play():
-    global weblink
+    """ Start playing stream on startup """
+    global WEB_LINK
     getplaying(channels[0])
-    weblink = (channels[0])
-    speakwhatson([channelname, whatson, weblink])
+    WEB_LINK = (channels[0])
+    speakwhatson([channelname, WHATS_ON, WEB_LINK])
 
 
-loadpage()
+if __name__ == '__main__':
+    """ Start up """
+    loadpage()
+    getchannels()
+    startup_play()
+    main()
 
-getchannels()
 
-startup_play()
-
-waitforbutton()
